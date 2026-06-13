a52522be8c291ff25cac929bd31b115c5658b1bc
mikitano <magakitano@yahoo.com.br>
2026-06-13 12:03:47 -0300

    add draw square test function

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mkitano <mkitano@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 17:40:43 by mkitano           #+#    #+#             */
-/*   Updated: 2026/06/04 17:55:24 by mkitano          ###   ########.fr       */
+/*   Updated: 2026/06/13 12:02:54 by mkitano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,28 @@ Abre a janela.
 Cria uma mlx_image_t.
 Adiciona a imagem na janela.
 Desenha um único pixel branco no centro (400, 300).
-Adicional: Desenhar a cruz inteira usando dois loops.*/
+Adicional: Desenhar a cruz inteira usando dois loops.
+Adicional: */
+
+void	draw_square( mlx_image_t *img, int x, int y, int size, uint32_t color)
+{
+	int	x2;
+	int	y2;
+
+	x2 = x;
+	y2 = y;
+
+	while (y2 < y + size)
+	{
+		while (x2 < x + size)
+		{
+			mlx_put_pixel(img, x2, y2, color);
+			x2++;
+		}
+		x2 = x;
+		y2++;
+	}
+}
 
 int	main(void)
 {
@@ -88,9 +109,10 @@ int	main(void)
 		mlx_put_pixel(img, 400, y, 0xFFFFFFFF);
 		y++;
 	}
+
+	//desenha quadrado
+	draw_square(img, 350, 250, 100, 0xFFFFFFFF);
 	mlx_loop(mlx);
 	mlx_delete_image(mlx, img);
 	mlx_terminate(mlx);
 }
-
-void	draw_square( mlx_image_t *img, int x, int y, int size, uint32_t color); //tentar desenhar um quadrado
