671e4575f3654c026ab7448d7f5473a3e4b402aa
mikitano <midori@MagaliDell22.localdomain>
2026-06-19 01:37:30 -0300

    delete objs

--- lib/libftx/obj/core/ft_putstr_fd.d
+++
@@ -1,6 +0,0 @@
-obj/core/ft_putstr_fd.o: src/core/ft_putstr_fd.c include/libft.h \
- include/ft_printf.h include/linked_list.h include/libft.h
-include/libft.h:
-include/ft_printf.h:
-include/linked_list.h:
-include/libft.h:
--- lib/libftx/obj/core/ft_strtrim.d
+++
@@ -1,6 +0,0 @@
-obj/core/ft_strtrim.o: src/core/ft_strtrim.c include/libft.h \
- include/ft_printf.h include/linked_list.h include/libft.h
-include/libft.h:
-include/ft_printf.h:
-include/linked_list.h:
-include/libft.h:
